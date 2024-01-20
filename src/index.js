0f9f20cd6e0159f011288858feddc8c6a5af20cb
25Ommy <156078030+25Ommy@users.noreply.github.com>
2024-01-20 15:20:37 -0800

    added some JS fetch using POST request

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ function sayWelcome() {
 }
 let heading = document.querySelector("button");
 heading.addEventListener("click", sayWelcome);
+
+let data = {
+  name: "Ommy",
+  surname: "Molele",
+  city: "Polokwane",
+  age: 23,
+};
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify(data),
+  headers: { "Content-type": "application/json" },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
